93c39b646a09c753ee75d0e44c401df6b1c9bdcf
Thileepan-Ilankumaran <112769282+Thileepan-Ilankumaran@users.noreply.github.com>
2024-08-10 00:18:08 +0530

    wallet setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AppKitProvider } from './AppKitProvider' // Import the provider
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <w3m-button />
+  </React.StrictMode>
+)
